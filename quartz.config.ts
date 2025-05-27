9ab65ad7b73a2d10596e7df303b7d2dee2bb1ce3
Justin W. Lin <29987540+justinlinw@users.noreply.github.com>
2025-05-26 18:00:12 -0700

    fix: set `defaultDateType` to created

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "winniexu.ca",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "modified",
+    defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
